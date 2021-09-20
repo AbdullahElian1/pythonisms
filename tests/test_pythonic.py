1ba4106ab6403578d20e210ef4d2e98eb2e58e64
AbdullahELian1 <abodelian28@gmail.com>
2021-09-20 14:54:20 +0300

    add unit test for str dunder init

--- tests/test_pythonic.py
+++ tests/test_pythonic.py
@@ -13,7 +13,8 @@ def data():
   list.insert(6)
   list.insert(1)
   return list
-  
 def test_len_of_list(data):
   assert len(data)==3
-  
+
+def test_dunder_str_method(data):
+    assert str(data)=="[1] -> [6] -> [2] -> None"
