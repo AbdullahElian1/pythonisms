eca8983f29963ec27d178132f8684d673c66b92c
AbdullahELian1 <abodelian28@gmail.com>
2021-09-20 14:53:14 +0300

    add fixture to test

--- tests/test_pythonic.py
+++ tests/test_pythonic.py
@@ -1,5 +1,15 @@
+import pytest
 from pythonic import __version__
-
+from pythonic.pyhtonic import *
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+@pytest.fixture
+def data():
+  list = LinkedList()
+  list.insert(2)
+  list.insert(6)
+  list.insert(1)
+  return list
